4d4f84565cb2432c6d420109febb367edc627130
Nurul Zaman <nurul.zaman@servicerocket.com>
2018-10-28 08:57:54 +0800

    rename methods to reflect crud, implement CRUD with local storage

--- main.js
+++ main.js
@@ -1,35 +1,53 @@
+//create a global variable by defining a key for local storage
+const TODO_DB = 'todo-records';
+
 new Vue({
-    //target main class wrapper
     el: ".todoapp",
-    //where data gets defined with its attributes
     data () {
         return {
-            //a "newTodo" object. The individual "todo" record that gets added to the whole todos
             newTodoRecord: '',
-            //a "todos" object which has to be any array later on
-            todos: [{
-                id: 0,
-                title: "Some title",
-                completed: 'false'
-            }]
+            todos: [],
+            editedTodo: null
         }
     },
+    //when a new Vue instance is being instantiated....
+    created() {
+      this.todos = JSON.parse(localStorage.getItem(TODO_DB) || '[]');
+    },
     methods: {
-        //defining a function that will dynamically add to the array list of the todos object
-        addTodo() {
+        createTodo() {
             //in words: access this "todos" data and update the following property
             this.todos.push({
                 id: this.todos.length,
                 title: this.newTodoRecord,
                 completed: false
             });
-            //after successfully entering a new record, clear the input
-            this.newTodoRecord = ''
+            this.newTodoRecord = '';
+            //update the local storage when record added
+            localStorage.setItem(TODO_DB, JSON.stringify(this.todos));
         },
-        removeTodo(todo) {
-            this.todos.splice(this.todos.indexOf(todo), 1)
+        deleteTodo(todo) {
+            this.todos.splice(this.todos.indexOf(todo), 1);
+            //update local storage when record removed
+            localStorage.setItem(TODO_DB, JSON.stringify(this.todos));
+        },
+        editTodo(todo) {
+            this.editedTodo = todo;
+        },
+        doneEdit(todo) {
+            //validation - if todo is empty, do nothing
+            if (!this.editedTodo) {
+                return
+            }
+            this.editedTodo = null;
+
+            //validation - if todo's title is empty, remove todo from the list to avoid empty todo
+            todo.title = todo.title.trim();
+            if (!todo.title) {
+                this.deleteTodo(todo);
+            }
+
+            localStorage.setItem(TODO_DB, JSON.stringify(this.todos));
         }
-
-
     }
 });
