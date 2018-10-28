227a15742cfab28d579bd55c4b5bd2399562fd3a
Nurul Zaman <nurul.zaman@servicerocket.com>
2018-10-28 10:28:28 +0800

    implement filter. pending - prevent edit once todo is done

--- main.js
+++ main.js
@@ -1,47 +1,64 @@
-//create a global variable by defining a key for local storage
 const TODO_DB = 'todo-records';
 
 new Vue({
     el: ".todoapp",
     data () {
         return {
+            //the data attributes and their default value as either '', [], null, 'all'
+            //these are the non-computed properties
             newTodoRecord: '',
             todos: [],
-            editedTodo: null
+            editedTodo: null,
+            visibility: 'all'
         }
     },
-    //when a new Vue instance is being instantiated....
     created() {
       this.todos = JSON.parse(localStorage.getItem(TODO_DB) || '[]');
     },
+    computed: {
+      filteredTodos() {
+          //which is filter by 'all', 'completed', 'active'
+          //condition 1: what to return when visibility is 'all'
+          if (this.visibility === 'all') {
+              return this.todos;
+          }
+          //condition 2: what to return when visibility is 'completed'
+          else if (this.visibility === 'active') {
+              return this.todos.filter(function (todo) {
+                  return !todo.completed;
+              })
+          }
+          //condition 3: what to return when visibility is 'active'
+          else {
+              return this.todos.filter(function (todo) {
+                  return todo.completed;
+              })
+          }
+      }
+    },
     methods: {
         createTodo() {
-            //in words: access this "todos" data and update the following property
             this.todos.push({
                 id: this.todos.length,
                 title: this.newTodoRecord,
                 completed: false
             });
             this.newTodoRecord = '';
-            //update the local storage when record added
             localStorage.setItem(TODO_DB, JSON.stringify(this.todos));
         },
         deleteTodo(todo) {
             this.todos.splice(this.todos.indexOf(todo), 1);
-            //update local storage when record removed
             localStorage.setItem(TODO_DB, JSON.stringify(this.todos));
         },
         editTodo(todo) {
             this.editedTodo = todo;
         },
         doneEdit(todo) {
-            //validation - if todo is empty, do nothing
             if (!this.editedTodo) {
                 return
             }
             this.editedTodo = null;
 
-            //validation - if todo's title is empty, remove todo from the list to avoid empty todo
             todo.title = todo.title.trim();
             if (!todo.title) {
                 this.deleteTodo(todo);
